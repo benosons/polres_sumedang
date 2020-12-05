7fb53eadeafa89eb0294f548b511eed8f4239c91
negasi <beno.sons@gmail.com>
2020-12-05 13:03:10 +0700

    update

--- application/Config/Database.php
+++ application/Config/Database.php
@@ -31,10 +31,10 @@ class Database extends \CodeIgniter\Database\Config
 	 */
 	public $default = [
 		'DSN'          => '',
-		'hostname'     => 'localhost',
-		'username'     => 'root',
-		'password'     => '12345',
-		'database'     => 'polres_sumedang',
+		'hostname'     => '',
+		'username'     => '',
+		'password'     => '',
+		'database'     => '',
 		'DBDriver'     => 'MySQLi',
 		'DBPrefix'     => '',
 		'pConnect'     => false,
