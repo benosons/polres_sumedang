f29a9eb07808b44f79f32532dfe4fea3cc0e695f
Beno Sons <beno.sons@gmail.com>
2022-03-31 22:09:07 +0700

    hapus komenan enggress

--- public/assets/action-js/users/informasi/berita-post.js
+++ public/assets/action-js/users/informasi/berita-post.js
@@ -125,106 +125,106 @@ function loadberita(param, id){
                   					</fieldset>
                   				</form>
                   			</div>`;
-          let contents = `<div class="row page_margin_top_section">
-                				<h4 class="box_header">6 Comments</h4>
-                				<ul id="comments_list">
-                					<li class="comment clearfix" id="comment-1">
-                						<div class="comment_author_avatar">
-                							&nbsp;
-                						</div>
-                						<div class="comment_details">
-                							<div class="posted_by clearfix">
-                								<h5><a class="author" href="index.html@page=post.html#" title="Kevin Nomad">Kevin Nomad</a></h5>
-                								<abbr title="22 July 2014" class="timeago">22 July 2014</abbr>
-                							</div>
-                							<p>
-                								Donec ipsum diam, pretium mollis dapibus risus. Nullam tindun pulvinar at interdum eget, suscipit eget felis. Pellentesque est faucibus tincidunt risus id interdum primis orci cubilla gravida id interdum eget.
-                							</p>
-                							<a class="read_more" href="index.html@page=post.html#comment_form" title="Reply">
-                								<span class="arrow"></span><span>REPLY</span>
-                							</a>
-                						</div>
-                					</li>
-                					<li class="comment clearfix" id="comment-2">
-                						<div class="comment_author_avatar">
-                							&nbsp;
-                						</div>
-                						<div class="comment_details">
-                							<div class="posted_by clearfix">
-                								<h5><a class="author" href="index.html@page=post.html#" title="Kevin Nomad">Kevin Nomad</a></h5>
-                								<abbr title="22 July 2014" class="timeago">22 July 2014</abbr>
-                							</div>
-                							<p>
-                								Donec ipsum diam, pretium mollis dapibus risus. Nullam tindun pulvinar at interdum eget, suscipit eget felis. Pellentesque est faucibus tincidunt risus id interdum primis orci cubilla gravida id interdum eget.
-                							</p>
-                							<a class="read_more" href="index.html@page=post.html#comment_form" title="Reply">
-                								<span class="arrow"></span><span>REPLY</span>
-                							</a>
-                						</div>
-                						<ul class="children">
-                							<li class="comment clearfix">
-                								<a href="index.html@page=post.html#comment-2" class="parent_arrow"></a>
-                								<div class="comment_author_avatar">
-                									&nbsp;
-                								</div>
-                								<div class="comment_details">
-                									<div class="posted_by clearfix">
-                										<h5><a class="author" href="index.html@page=post.html#" title="Keith Douglas">Keith Douglas</a><a href="index.html@page=post.html#comment-2" class="in_reply">@Kevin Nomad</a></h5>
-                										<abbr title="22 July 2014" class="timeago">22 July 2014</abbr>
-                									</div>
-                									<p>
-                										Donec ipsum diam, pretium mollis dapibus risus. Nullam tindun pulvinar at interdum eget, suscipit eget felis. Pellentesque est faucibus tincidunt risus id interdum primis orci cubilla gravida id interdum eget.
-                									</p>
-                									<a class="read_more" href="index.html@page=post.html#comment_form" title="Reply">
-                										<span class="arrow"></span><span>REPLY</span>
-                									</a>
-                								</div>
-                							</li>
-                							<li class="comment clearfix">
-                								<a href="index.html@page=post.html#comment-2" class="parent_arrow"></a>
-                								<div class="comment_author_avatar">
-                									&nbsp;
-                								</div>
-                								<div class="comment_details">
-                									<div class="posted_by clearfix">
-                										<h5><a class="author" href="index.html@page=post.html#" title="Keith Douglas">Keith Douglas</a><a href="index.html@page=post.html#comment-2" class="in_reply">@Kevin Nomad</a></h5>
-                										<abbr title="22 July 2014" class="timeago">22 July 2014</abbr>
-                									</div>
-                									<p>
-                										Donec ipsum diam, pretium mollis dapibus risus. Nullam tindun pulvinar at interdum eget, suscipit eget felis. Pellentesque est faucibus tincidunt risus id interdum primis orci cubilla gravida id interdum eget.
-                									</p>
-                									<a class="read_more" href="index.html@page=post.html#comment_form" title="Reply">
-                										<span class="arrow"></span><span>REPLY</span>
-                									</a>
-                								</div>
-                							</li>
-                						</ul>
-                					</li>
-                				</ul>
-                				<ul class="pagination page_margin_top_section">
-                					<li class="left">
-                						<a href="index.html@page=post.html#" title="">&nbsp;</a>
-                					</li>
-                					<li class="selected">
-                						<a href="index.html@page=post.html#" title="">
-                							1
-                						</a>
-                					</li>
-                					<li>
-                						<a href="index.html@page=post.html#" title="">
-                							2
-                						</a>
-                					</li>
-                					<li>
-                						<a href="index.html@page=post.html#" title="">
-                							3
-                						</a>
-                					</li>
-                					<li class="right">
-                						<a href="index.html@page=post.html#" title="">&nbsp;</a>
-                					</li>
-                				</ul>
-                			</div>`;
+        //   content += `<div class="row page_margin_top_section">
+        //         				<h4 class="box_header">6 Comments</h4>
+        //         				<ul id="comments_list">
+        //         					<li class="comment clearfix" id="comment-1">
+        //         						<div class="comment_author_avatar">
+        //         							&nbsp;
+        //         						</div>
+        //         						<div class="comment_details">
+        //         							<div class="posted_by clearfix">
+        //         								<h5><a class="author" href="index.html@page=post.html#" title="Kevin Nomad">Kevin Nomad</a></h5>
+        //         								<abbr title="22 July 2014" class="timeago">22 July 2014</abbr>
+        //         							</div>
+        //         							<p>
+        //         								Donec ipsum diam, pretium mollis dapibus risus. Nullam tindun pulvinar at interdum eget, suscipit eget felis. Pellentesque est faucibus tincidunt risus id interdum primis orci cubilla gravida id interdum eget.
+        //         							</p>
+        //         							<a class="read_more" href="index.html@page=post.html#comment_form" title="Reply">
+        //         								<span class="arrow"></span><span>REPLY</span>
+        //         							</a>
+        //         						</div>
+        //         					</li>
+        //         					<li class="comment clearfix" id="comment-2">
+        //         						<div class="comment_author_avatar">
+        //         							&nbsp;
+        //         						</div>
+        //         						<div class="comment_details">
+        //         							<div class="posted_by clearfix">
+        //         								<h5><a class="author" href="index.html@page=post.html#" title="Kevin Nomad">Kevin Nomad</a></h5>
+        //         								<abbr title="22 July 2014" class="timeago">22 July 2014</abbr>
+        //         							</div>
+        //         							<p>
+        //         								Donec ipsum diam, pretium mollis dapibus risus. Nullam tindun pulvinar at interdum eget, suscipit eget felis. Pellentesque est faucibus tincidunt risus id interdum primis orci cubilla gravida id interdum eget.
+        //         							</p>
+        //         							<a class="read_more" href="index.html@page=post.html#comment_form" title="Reply">
+        //         								<span class="arrow"></span><span>REPLY</span>
+        //         							</a>
+        //         						</div>
+        //         						<ul class="children">
+        //         							<li class="comment clearfix">
+        //         								<a href="index.html@page=post.html#comment-2" class="parent_arrow"></a>
+        //         								<div class="comment_author_avatar">
+        //         									&nbsp;
+        //         								</div>
+        //         								<div class="comment_details">
+        //         									<div class="posted_by clearfix">
+        //         										<h5><a class="author" href="index.html@page=post.html#" title="Keith Douglas">Keith Douglas</a><a href="index.html@page=post.html#comment-2" class="in_reply">@Kevin Nomad</a></h5>
+        //         										<abbr title="22 July 2014" class="timeago">22 July 2014</abbr>
+        //         									</div>
+        //         									<p>
+        //         										Donec ipsum diam, pretium mollis dapibus risus. Nullam tindun pulvinar at interdum eget, suscipit eget felis. Pellentesque est faucibus tincidunt risus id interdum primis orci cubilla gravida id interdum eget.
+        //         									</p>
+        //         									<a class="read_more" href="index.html@page=post.html#comment_form" title="Reply">
+        //         										<span class="arrow"></span><span>REPLY</span>
+        //         									</a>
+        //         								</div>
+        //         							</li>
+        //         							<li class="comment clearfix">
+        //         								<a href="index.html@page=post.html#comment-2" class="parent_arrow"></a>
+        //         								<div class="comment_author_avatar">
+        //         									&nbsp;
+        //         								</div>
+        //         								<div class="comment_details">
+        //         									<div class="posted_by clearfix">
+        //         										<h5><a class="author" href="index.html@page=post.html#" title="Keith Douglas">Keith Douglas</a><a href="index.html@page=post.html#comment-2" class="in_reply">@Kevin Nomad</a></h5>
+        //         										<abbr title="22 July 2014" class="timeago">22 July 2014</abbr>
+        //         									</div>
+        //         									<p>
+        //         										Donec ipsum diam, pretium mollis dapibus risus. Nullam tindun pulvinar at interdum eget, suscipit eget felis. Pellentesque est faucibus tincidunt risus id interdum primis orci cubilla gravida id interdum eget.
+        //         									</p>
+        //         									<a class="read_more" href="index.html@page=post.html#comment_form" title="Reply">
+        //         										<span class="arrow"></span><span>REPLY</span>
+        //         									</a>
+        //         								</div>
+        //         							</li>
+        //         						</ul>
+        //         					</li>
+        //         				</ul>
+        //         				<ul class="pagination page_margin_top_section">
+        //         					<li class="left">
+        //         						<a href="index.html@page=post.html#" title="">&nbsp;</a>
+        //         					</li>
+        //         					<li class="selected">
+        //         						<a href="index.html@page=post.html#" title="">
+        //         							1
+        //         						</a>
+        //         					</li>
+        //         					<li>
+        //         						<a href="index.html@page=post.html#" title="">
+        //         							2
+        //         						</a>
+        //         					</li>
+        //         					<li>
+        //         						<a href="index.html@page=post.html#" title="">
+        //         							3
+        //         						</a>
+        //         					</li>
+        //         					<li class="right">
+        //         						<a href="index.html@page=post.html#" title="">&nbsp;</a>
+        //         					</li>
+        //         				</ul>
+        //         			</div>`;
 
           $('#post-berita').append(content);
       }
